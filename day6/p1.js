1f553eb4d3d204a40f3988220f7f4572a691c030
Coder Gautam <gautamgxtv@gmail.com>
2022-12-05 23:07:47 -0600

    day6

---
+++ day6/p1.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8').split('')
+let f = false;
+input.forEach((char,i) => {
+  // check past 4 chars
+  let past4 = input.slice(i-4,i)
+  if(past4.length < 4) return;
+  let past4Str = past4.join('')
+  // check if 4 are all different
+  if(past4Str.length != new Set(past4Str).size || f) return;
+  console.log(i)
+  f = true;
+});
+
+console.log(input)
