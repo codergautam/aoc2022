06540a2003a70678e416ecd682bbaeb81c26015c
Coder Gautam <gautamgxtv@gmail.com>
2022-12-06 23:31:04 -0600

    day 7

---
+++ day7/p2.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8').split('\n')
+
+let tree = {files: []};
+let cwd = [];
+input.forEach((line,i) => {
+  if(line.startsWith("$ cd")) {
+    let a = line.split(" ")[2]
+    if(a == "..") {
+      cwd = cwd.slice(0, cwd.length-1)
+    } else if (a == "/") {
+      cwd = []
+    } else {
+      cwd.push(a)
+    }
+
+    // access nested tree based on cwd
+    let current = tree;
+    cwd.forEach((item) => {
+      if(!current[item]) current[item] = {files: []}
+      current = current[item]
+    })
+  } else if(line.startsWith("$ ls")) {
+    // get next x lines that dont start with $
+    let nextLines = [];
+    for(let j = i+1; j < input.length; j++) {
+      if(input[j].startsWith("$")) break;
+      nextLines.push(input[j])
+    }
+    nextLines.forEach((item) => {
+      if(item.startsWith("dir")) {
+        // access nested tree based on cwd
+        let current = tree;
+        cwd.forEach((item) => {
+          if(!current[item]) current[item] = {files: []}
+          current = current[item]
+        });
+        current[item.split(" ")[1]] = {files: []}
+      } else {
+        // access nested tree based on cwd
+        const [size, name] = item.split(" ")
+        let current = tree;
+        cwd.forEach((item) => {
+          if(!current[item]) current[item] = {files: []}
+          current = current[item]
+        });
+        current.files.push({name, size})
+      }
+    });
+  }
+})
+
+// Find all directories size
+function findSize(tree) {
+  let size = 0;
+  tree.files.forEach((item) => {
+    size += parseInt(item.size)
+  })
+  Object.values(tree).forEach((item) => {
+    if(item.files) {
+      size += findSize(item)
+    }
+  })
+  return size;
+}
+let possible = [];
+const totalUsed = findSize(tree)
+function find(tree) {
+Object.keys(tree).forEach((item) => {
+  if(item == "files") return;
+  if((70000000 - totalUsed) + findSize(tree[item]) > 30000000 ) possible.push(findSize(tree[item]))
+  find(tree[item])
+});
+}
+find(tree)
+
+console.log( possible.sort((a,b) => a-b)[0])
